1e8656b6f6d7cfd2b523d53db6f865b3113a1f9c
abdallahbilal <105009265+abdallahbilal@users.noreply.github.com>
2022-10-02 17:20:35 -0400

    Add project files.

---
+++ Models/Players.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Assignment2.Models
+{
+    public class Players
+    {
+        [Required]
+        public int PlayerNumber { get; set; }
+
+
+        [Required]
+        public string PlayerName { get; set; }
+
+
+        [Required]
+        public string Country { get; set; }
+
+
+        [Required]
+        public int Age { get; set; }
+
+
+        [Required]
+        public int PurchasePrice { get; set; }
+
+
+
+
+
+
+    }
+}
---
+++ Models/Teams.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Assignment2.Models
+{
+    public class Teams
+    {
+        [Required]
+        public int TeamId { get; set; }
+
+
+        [Required]
+        public string TeamName { get; set; }  
+        
+
+        [Required]
+        public string TeamCoach { get; set; }
+
+
+        [Required]
+        public int Budget { get; set; }
+
+    }
+}
