b8b5e76d8deec0d46fe2bcc8aefe879977c84b72
abdallahbilal <105009265+abdallahbilal@users.noreply.github.com>
2022-10-12 21:33:07 -0400

    Commint 13

---
+++ Data/Migrations/20221012215119_NewFinal.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Assignment2.Data.Migrations
+{
+    public partial class NewFinal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_Teams_TeamId",
+                table: "Players");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Players_TeamId",
+                table: "Players");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TeamId",
+                table: "Players",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TeamsTeamId",
+                table: "Players",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_TeamsTeamId",
+                table: "Players",
+                column: "TeamsTeamId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_Teams_TeamsTeamId",
+                table: "Players",
+                column: "TeamsTeamId",
+                principalTable: "Teams",
+                principalColumn: "TeamId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_Teams_TeamsTeamId",
+                table: "Players");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Players_TeamsTeamId",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "TeamsTeamId",
+                table: "Players");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TeamId",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_TeamId",
+                table: "Players",
+                column: "TeamId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_Teams_TeamId",
+                table: "Players",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "TeamId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
